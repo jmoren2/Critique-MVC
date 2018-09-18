628b13c4f0cac5ddb3ddeb09be816437f13948af
Josh Moreno <jmoreno@navexgloabl.com>
2018-09-18 13:12:52 -0700

    initial commit 1

---
+++ CritiqueFoodReviewer/Models/AboutModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CritiqueFoodReviewer.Models
+{
+    public class AboutModel
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+
+        public string jobtitle;
+
+        public string JobTitle {
+            get
+            {
+                return jobtitle;
+            }
+
+            set
+            {
+
+                if (!String.IsNullOrEmpty(value))
+                {
+                    jobtitle = "Software Engineer";
+
+                }
+            }
+        }
+    }
+}
